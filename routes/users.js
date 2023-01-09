acd3f874819045cdb117ac8742d84d31df6cdb40
NAKUL  SINGH <nakulsingh@192.168.1.58>
2023-01-10 02:27:28 +0530

    deployment

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.put('/sub/:id', subscribe)
 router.put('/unsub/:id', unsubscribe)
 
 //like a video
-router.put('/like/:videoId', verifyToken, like)
+router.put('/like/:videoId', like)
 
 //dislike a video
 router.put('/dislike/:videoId', dislike)
