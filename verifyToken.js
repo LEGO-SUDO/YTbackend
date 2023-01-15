a09589b883d61fed4d8d20d87da9f5e6315397d7
NAKUL  SINGH <nakulsingh@192.168.1.4>
2023-01-15 23:58:43 +0530

    layout fixing

--- verifyToken.js
+++ verifyToken.js
@@ -5,10 +5,10 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token
   console.log(req.cookies)
 
-  // if (!token) return next(createError(401, 'You are not authenticated'))
-  // jwt.verify(token, process.env.JWT, (err, user) => {
-  //   if (err) return next(createError(403, 'Token is not Valid'))
-  //   req.user = user
-  //   next()
-  //})
+  if (!token) return next(createError(401, 'You are not authenticated'))
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, 'Token is not Valid'))
+    req.user = user
+    next()
+  })
 }
